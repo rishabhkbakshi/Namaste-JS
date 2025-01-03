d07b29e0685267c682926db85f2f30073f70d744
rishabhkbakshi <bakshirishab90@gmail.com>
2025-01-03 18:14:13 +0530

    proper commenting in both the concept

--- FundamentalJS/js/deboucing-concept.js
+++ FundamentalJS/js/deboucing-concept.js
@@ -1,22 +1,34 @@
 //Debouncing in JAVASCRIPT
 
+/**
+ * Debouncing is a technique in JavaScript that limits the rate at which a function can be executed. 
+ * It's commonly used to improve website performance and user experience by preventing a function 
+ * from firing too frequently in response to events like scrolling, resizing, or typing.
+ */
 let counter = 0;
 let getData = () => {
     // calls an API and gets the data
     console.log('Fetching data...', counter++);
 }
 
+/**
+ * 
+ * @param {This function represent the getData function} fn 
+ * @param {It represent the specified delay after that the event will trigger } delay 
+ * @returns the inner function which actually debounced the event
+ */
 let debouce = function (fn, delay) {
     let timer;
     return function () {
-        // This will be executed after delay milliseconds
-        let context = this; // here this is the window object, 
+        let context = this; // here this is the window object 
         // as we know that 'this' in arrow function represent the lexical parent object.
-        let args = arguments; // 
+        let args = arguments; // arguments => to handle the arguments if it call.
         // arguments is an array-like object accessible inside functions that contains the values of the arguments passed to that function. 
-        clearTimeout(timer);
+        clearTimeout(timer); // clear the timer
+
+        // This will be executed after delay milliseconds
         timer = setTimeout(() => {
-            fn.apply(context, args);
+            fn.apply(context, args); // getData method will be called
         }, delay);
     }
 }
--- FundamentalJS/js/throttling-concept.js
+++ FundamentalJS/js/throttling-concept.js
@@ -1,20 +1,45 @@
+/**
+ * First of all what is throttling in JS => 
+ * Throttling is a technique used to control the rate at which a function is executed. 
+ * It ensures that the function is called at most once within a specified time interval, 
+ * regardless of how many times the event triggering the function is fired
+ */
+
 //  Before throttled
 const expensive = () => {
     console.log('Expensive');
 }
 
-window.addEventListener('resize', expensive);
+// window.addEventListener('resize', expensive);// This line will execute when window is resized
 
 // Throttled version
+/**
+ * 
+ * @param {This function represent the expensive function} fn 
+ * @param {It represent the specified time interval after that the event will trigger } limit 
+ * @returns the inner function which actually throttled
+ */
 const throttle = (fn, limit) => {
+    /**
+     * we can achieve throttling by using the concept of closure. (Closure basically it will attached the flag variable to the inner function)
+     * flag is a boolean variable to check if the function should be executed or not.
+     * true means function should be executed.
+     * false means function should be ignored.
+     */
     let flag = true;
     return function () {
-        console.log(arguments);
-        let context = this;
-        let args = arguments;
+        let context = this; // here this is the window object 
+        // as we know that 'this' in arrow function represent the lexical parent object.
+        let args = arguments; // arguments => to handle the arguments if it call.
+        // arguments is an array-like object accessible inside functions that contains the values of the arguments passed to that function. 
         if (flag) {
-            fn.apply(context, args);
-            flag = false;
+            fn.apply(context, args); // here expensible method will be called
+            flag = false; // flag will be false just after the execution of exepensive method
+
+            /**
+             * this code will set the flag value to true 
+             * it means now is the time to call the function again after certain limit of time has passed
+             */
             setTimeout(() => {
                 flag = true;
             }, limit);
@@ -24,4 +49,5 @@ const throttle = (fn, limit) => {
 
 const betterExpensive = throttle(expensive, 5000);
 
-window.addEventListener('resize', betterExpensive);
+window.addEventListener('resize', betterExpensive); // This line will execute when window is resized
+// betterExpensive will execute throttle function when window is resized
