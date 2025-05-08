1ea0b51aebc3468ff0fc28bd352a6aa7c52490a4
rishabhkbakshi <bakshirishab90@gmail.com>
2025-05-08 18:31:03 +0530

    Enhances documentation in polyfill-for-bind-method.js and prototypal-inheritance.js for better clarity and understanding of concepts and usage.

--- FundamentalJS/js/polyfill-for-bind-method.js
+++ FundamentalJS/js/polyfill-for-bind-method.js
@@ -1,3 +1,10 @@
+/**
+ * Polyfill for bind method
+ * The bind() method creates a new function that, when called, has its this keyword set to the provided value,
+ * with a given sequence of arguments preceding any provided when the new function is called.
+ * It allows you to set the value of this in a function, which is particularly useful when passing a function as a callback.
+ * 
+ */
 let nameObject = {
     firstName: 'Rishabh',
     lastName: 'Bakshi'
@@ -13,29 +20,45 @@ printNameWithBind(); // Output: Rishabh Bakshi
 
 
 // Using custom bind method 
+// The custom bind method is a way to create a new function that, when called, has its this keyword set to the provided value,
+// with a given sequence of arguments preceding any provided when the new function is called.
+// It allows you to set the value of this in a function, which is particularly useful when passing a function as a callback.
 Function.prototype.mybind = function (...args) {
-    let obj = this;
+    let obj = this; // here 'this' is the function which is calling the mybind method
     // when we have one parameter on function calling
     // return function () {
-    //     obj.call(args[0]);
+    //     obj.call(args[0]); // here args[0] is the first parameter which is passed in mybind method
     // }
 
     // when we have more than one parameter on .custombind (like mybind) method
     // let params = args.slice(1); // .slice(1) - it will return all the elements of array except the first one
     // return function () {
-    //     obj.apply(args[0], params);
+    //     obj.apply(args[0], params); // here args[0] is the first parameter which is passed in mybind method and params are the other parameters which are passed in mybind method
     // }
-    
+
     // when we have more than one parameter along with passing parameter in method calling which are returned by .custombind (like mybind) method
-    let params = args.slice(1);
+    let params = args.slice(1); // .slice(1) - it will return all the elements of array except the first one
     /**
      * here ...arg2 represent the all parameters in the calling methods which are returned by .custombind (like mybind) method
      */
     return function (...args2) {
-        let finalArr = [...params, ...args2];
-        obj.apply(args[0], finalArr);
+        // Approach 1
+        // args2.forEach((param) => {
+        //     params.push(param); // here we are pushing the args2 array elements in params array
+        // });
+        // obj.apply(args[0], params);
+
+        // Approach 2 - using concat method
+        // let finalArr = params.concat(args2); // here we are merging the params and args2 array using concat method
+        // obj.apply(args[0], finalArr); 
+
+        // Approach 3 - simple and easy to understand - using spread operator
+        let finalArr = [...params, ...args2]; // here we are merging the params and args2 array using spread operator
+        obj.apply(args[0], finalArr); // here args[0] is the first parameter which is passed in mybind method and finalArr are the other parameters which are passed in mybind method
     }
 }
 
+// when we have one parameter on function calling
 let printNameWithBind2 = printName.mybind(nameObject, 'Rajasthan');
-printNameWithBind2('India'); // Output: Rishabh
+// when we have more than one parameter on function calling
+printNameWithBind2('India'); // Output: Rishabh Bakshi from Rajasthan (India)
--- FundamentalJS/js/prototypal-inheritance.js
+++ FundamentalJS/js/prototypal-inheritance.js
@@ -1,16 +1,18 @@
 
 /**
- * Here explaning what is prototypal inheritance 
- * 
+ * Here explaning what is prototypal inheritance :-  
+ * Prototypal inheritance is a feature in JavaScript that allows objects to inherit properties and methods from other objects.
+ * It is a way to create new objects based on existing objects, allowing for code reuse and the creation of complex data structures.
+ *
  */
 let arr = ['Rishabh', 'Rahul', 'Atul', 'Abhay'];
 
 //  Now let's check the prototype of arr and Array.prototype
-console.log(arr.__proto__);
+console.log(arr.__proto__); // it will print the prototype of arr
 console.log(Array.prototype); // both will print the same prototype
 
 // Now we do like this
-console.log(arr.__proto__.__proto__);
+console.log(arr.__proto__.__proto__); // it will print the prototype of Array.prototype
 console.log(Object.prototype); // both will print the same prototype
 // it means arr.__proto__ is the Array prototype and arr.__proto__.__proto__ is the Object prototype
 
@@ -49,9 +51,9 @@ console.log(object.__proto__.__proto__); // it will print 'null'. But why ?
 function fun() {
 
 }
-// in case funtion
+// in case of function
 console.log(fun.__proto__);
-console.log(Function.prototype); // both will print the funtion prototype
+console.log(Function.prototype); // both will print the function prototype
 
 // But we do like that
 console.log(fun.__proto__.__proto__); // it will print 'Object'
@@ -68,6 +70,9 @@ let object2 = {
 /**
  * This line of code is not advisable
  * It will severely affect on the performance of your program
+ * But this is how prototypal inheritance works in JavaScript
+ * It means that object2 is inheriting the properties of object
+ * 
  */
 // if we do like this 
 object2.__proto__ = object;
@@ -112,11 +117,17 @@ obj2.printNewMethod(); // It will print 'This is new method'
 let obj3 = Object.create(obj2);
 obj3.printNewMethod(); // It will print 'This is new method'
 
-//  Note: In JavaScript, every object has a prototype property that links to another object.
-//  The prototype object is used to provide shared properties and methods for all objects created from it.
-
+// Note: In JavaScript, every object has a prototype property that links to another object.
+// The prototype object is used to provide shared properties and methods for all objects created from it.
+// This allows for code reuse and the creation of complex data structures.
+// The prototype chain is the mechanism by which JavaScript objects inherit features from one another.
+// When you access a property or method on an object, JavaScript first checks if the property exists on the object itself. If it doesn't, it looks up the prototype chain until it finds the property or reaches the end of the chain (null).
+// This is how prototypal inheritance works in JavaScript.
 
 // in terms of function
+//  We can also add a new method to function's prototype
+//  This method will be available to all functions
+//  This is a test method which will be available to all functions
 Function.prototype.testBind = function () {
     console.log('This is a test method');
 }
